c68212e7fd67e98ecf6494327eb6784b2b805735
Bartek <118764028+barteqcz@users.noreply.github.com>
2026-05-17 17:13:44 +0200

    Create pm.sh

---
+++ pm.sh
@@ -0,0 +1,20 @@
+# Disable power management at boot for the wifi card (useful for rtw89 cards)
+
+#!/bin/bash
+
+wifi_card_name="wlan0"
+
+sudo tee /etc/systemd/system/wif-powersave-off.service > /dev/null <<EOF
+[Unit]
+Description=Disable WiFi power saving
+After=network.target
+
+[Service]
+Type=oneshot
+ExecStartPre=/bin/sleep 20
+ExecStart=/usr/sbin/iw dev $wifi_card_name set power_save off
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
